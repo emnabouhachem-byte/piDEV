786edd6caffb2d0d75d2bd3e75b7315389e92bb2
emnabouhachem-byte <emna.bouhachem@esprit.tn>
2026-02-07 16:29:18 +0100

    CRUD terminé

---
+++ project - Copie 1/src/main/java/edu/project/tests/MainClass.java
@@ -0,0 +1,12 @@
+package edu.project.tests;
+
+import edu.project.tools.MyConnection;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        MyConnection mc = new MyConnection();
+    }
+}
+
+
---
+++ project - Copie 1/src/main/java/edu/project/tools/MyConnection.java
@@ -0,0 +1,25 @@
+package edu.project.tools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+
+    public String url="jdbc:mysql://localhost:3306/pidev";
+    public String login="root";
+    public String pwd ="";
+
+
+    private Connection cnx;
+    public MyConnection() {
+        try {
+           cnx = DriverManager.getConnection(url,login,pwd);
+            System.out.println("connection établie!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
